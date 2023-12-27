19a03c89f55ae08dc7a26731daeeffcb54bb123a
capTsubasa10 <ashwinraaj1093@gmail.com>
2023-12-27 11:33:31 +0530

    First time commit

---
+++ src/main/java/com/nithya/AddServlet.java
@@ -0,0 +1,25 @@
+package com.nithya;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j ;
+		
+		PrintWriter output = res.getWriter();
+		
+		output.println("Addition result = " + k);
+		
+	}
+
+}
